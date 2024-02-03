b82afa35984bc36d9a520fb1933f715dbdf5ff7e
mvelasquezmontoya <martin.velasquez.montoya@gmail.com>
2024-02-03 15:13:13 -0500

    Documento dividido en secciones.

--- scripts/Script.R
+++ scripts/Script.R
@@ -1,1 +1,45 @@
-#Script
+# 
+# Big Data y Machine Learning para Economia Aplicada
+# Maria Camila Arias, Martin Velasquez, Mario Velasquez, Daniela Vlasak
+# Problem Set 1
+# 
+
+# Initial Setup -----------------------------------------------------------
+
+if(!require(pacman)) install.packages("pacman") ; require(pacman)
+
+
+p_load(rio, # import/export data
+       tidyverse, # tidy-data
+       skimr, # summary data
+       gridExtra, ## visualizing missing data
+       corrplot, ## Correlation Plots 
+       stargazer, ## tables/output to TEX. 
+       MASS)   
+
+
+
+# Initial Data Manipulation -----------------------------------------------
+
+cd <- import("https://ignaciomsarmiento.github.io/GEIH2018_sample/")
+
+
+
+
+# Variables and Descriptive Statistics ------------------------------------
+
+
+
+
+
+
+# Age-wage Profile --------------------------------------------------------
+
+
+
+
+
+
+# The Gender Earnings GAP -------------------------------------------------
+
+
