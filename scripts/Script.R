1e095e62d641b0b0c5d239282f2f2d6c8dae03f9
Carialva <mc.arias1074@uniandes.edu.co>
2024-02-10 11:34:02 -0500

    Update Script.R

--- scripts/Script.R
+++ scripts/Script.R
@@ -9,6 +9,10 @@
 if(!require(pacman)) install.packages("pacman") ; require(pacman)
 library(tidyverse)
 
+p_load(tidyverse, # contiene las librerías ggplot, dplyr...
+       rvest) # web-scraping
+vignette("rvest")
+
 
 p_load(rio, # import/export data
        tidyverse, # tidy-data
@@ -20,7 +24,28 @@ p_load(rio, # import/export data
 
 # Initial Data Manipulation -----------------------------------------------
 
-bd <- import("https://ignaciomsarmiento.github.io/GEIH2018_sample/")
+my_url="https://ignaciomsarmiento.github.io/GEIH2018_sample/"
+browseURL(my_url)
+my_html = read_html(my_url)
+
+## I get the links of each chunk of data
+links<- my_html %>%
+  html_elements("ul") %>%
+  html_elements("a") %>% 
+  html_attr("href") %>%
+  keep( ~ grepl("page",.))
+## I paste the first part of the address to the links
+links<- paste0(my_url, links)
+
+
+## I want to open each page and extract their table
+## I create a function to fetch HTML content from a url I give as input
+fetch_html <- function(url) {
+  read_html(url)
+}
+
+## Now I loop through each link and fetch the HTML content
+html_content_list <- lapply(links,fetch_html)
 
 
 
